47e671e6771fcfa43c280cb6393e952e42636eb0
Davin Lagerroos <dlagerroos@ssa-i.org>
2013-10-17 09:21:08 -0500

    remove unused Commands class

--- lib/redis_backed_model.rb
+++ lib/redis_backed_model.rb
@@ -3,7 +3,6 @@ require 'active_support/inflector'
 
 require 'redis_backed_model/redis_backed_model'
 require 'redis_backed_model/inflections'
-require 'redis_backed_model/commands'
 require 'redis_backed_model/sorted_set'
 require 'redis_backed_model/redis_set'
 require 'redis_backed_model/redis_hash'
